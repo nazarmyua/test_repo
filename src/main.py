18d93aaeba1b933326e37a935329ca01dc4fd3ce
Nazar Ostapchuk <nazaro@neogames.com>
2025-11-10 18:07:29 +0200

    update

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def main():
+
+    print("ini kfgjfkldjg fdjkgkldf jg lkjdfklg jldfkjg klfj gkldfj gkjdf klfdjg k jd fklg jdflkgj kldfjgk lsdfjg klsdfjlgkjsdfklg j;sdlfkjg klsdfj glksdjflkg j dflk gt")
+
+
+if __name__ == "__main__":
+    main()
